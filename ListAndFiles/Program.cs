e3464e98c9918e5353d273146e95370d2ad0c170
Opilane TTHK <opilane@tthk.ee>
2020-10-29 11:30:40 +0200

    first commit

---
+++ ListAndFiles/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListAndFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> fruit = new List<string>();
+
+            for(int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("enter a fruit: ");
+                fruit.Add(Console.ReadLine());
+            }
+            foreach(string fruitItem in fruit)
+            {
+                Console.WriteLine($"fruit on the list: {fruitItem}");
+            }
+
+        }
+    }
+}
